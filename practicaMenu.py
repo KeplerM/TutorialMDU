6edb7de448f0f228909cd705dad11bcf944b9adf
KeplerM <diego-32-26@hotmail.com>
2023-05-23 09:33:20 -0500

    Creation pop-up windows, Tutorial: Curso python 53 - Interfaces Graficas XII

--- practicaMenu.py
+++ practicaMenu.py
@@ -1,6 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 
 root=Tk()
+
+def infoAdicional():
+    messagebox.showinfo("Procesador de Diego", "Procesador de textos version 2023")
+    
+def avisoLicencia():
+    messagebox.showwarning("Licencia", "Producto bajo licencia GNU" )
+
+def salirAplicacion():
+    #valor=messagebox.askquestion("Salir", "¿Deseas salir de la aplicación?")
+    valor=messagebox.askokcancel("Salir", "¿Deseas salir de la aplicación?")
+
+    if valor == True:
+        root.destroy()
+
+def cerrarDocumento():
+    valor=messagebox.askretrycancel("Reintentar", "No es posible cerrar documento bloqueado")
+    
+    if valor == False:
+            root.destroy()
+
 
 barraMenu=Menu(root)
 root.config(menu=barraMenu, width=300, height=300)
@@ -10,8 +31,8 @@ archivoMenu.add_command(label="Nuevo")
 archivoMenu.add_command(label="Guardar")
 archivoMenu.add_command(label="Guardar como")
 archivoMenu.add_separator()
-archivoMenu.add_command(label="Cerrar")
-archivoMenu.add_command(label="Salir")
+archivoMenu.add_command(label="Cerrar", command=cerrarDocumento)
+archivoMenu.add_command(label="Salir", command=salirAplicacion)
 
 archivoEdicion=Menu(barraMenu, tearoff=0)
 archivoEdicion.add_command(label="Copiar")
@@ -21,8 +42,8 @@ archivoEdicion.add_command(label="Pegar")
 archivoHerramientas=Menu(barraMenu, tearoff=0)
 
 archivoAyuda=Menu(barraMenu, tearoff=0)
-archivoAyuda.add_command(label="Licencia")
-archivoAyuda.add_command(label="Acerca de ...")
+archivoAyuda.add_command(label="Licencia", command=avisoLicencia)
+archivoAyuda.add_command(label="Acerca de ...", command=infoAdicional)
 
 barraMenu.add_cascade(label="Archivo", menu=archivoMenu)
 
