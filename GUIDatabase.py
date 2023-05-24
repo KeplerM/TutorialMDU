fa45709e2c75b79bf53d12fdceed15db2b7cca64
KeplerM <diego-32-26@hotmail.com>
2023-05-24 12:30:32 -0500

    GUIDatabase3:45:32, Tkinter Course - User Interfaces in Phyton Tutorial

---
+++ GUIDatabase.py
@@ -0,0 +1,108 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import filedialog
+import sqlite3
+
+root=Tk()
+root.title("Open Files Dialog Box")
+root.iconbitmap("AMADEico.ico")
+#root.geometry("400x400")
+
+#Databases
+#Create a database or connect to one
+conn=sqlite3.connect('address_book.db')
+
+#Create cursor
+c=conn.cursor()
+
+#Create table
+'''
+c.execute("""CREATE TABLE addresses (
+        first_name text,
+        last_name text,
+        address text,
+        city text,
+        state text,
+        zipcode integer
+        )""")
+'''
+#Create submit function for data base
+
+def submit():
+
+    #Create a database or connect to one
+    conn=sqlite3.connect('address_book.db')
+    #Create cursor
+    c=conn.cursor() 
+
+    #Insert Into Table
+    c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+              {
+                  'f_name': f_name.get(),
+                  'l_name': l_name.get(),
+                  'address': address.get(),
+                  'city': city.get(),
+                  'state': state.get(),
+                  'zipcode': zipcode.get()
+              })   
+              
+    #Commit changes
+    conn.commit()
+    #Close connection
+    conn.close()
+
+    #Clear the text boxes
+    f_name.delete(0,END)
+    l_name.delete(0,END)
+    address.delete(0,END)
+    city.delete(0,END)
+    state.delete(0,END)
+    zipcode.delete(0,END)
+
+
+#Create Text Boxes
+f_name=Entry(root, width=30)
+f_name.grid(row=0, column=1, padx=20)
+
+l_name=Entry(root, width=30)
+l_name.grid(row=1, column=1, padx=20)
+
+address=Entry(root, width=30)
+address.grid(row=2, column=1, padx=20)
+
+city=Entry(root, width=30)
+city.grid(row=3, column=1, padx=20)
+
+state=Entry(root, width=30)
+state.grid(row=4, column=1, padx=20)
+
+zipcode=Entry(root, width=30)
+zipcode.grid(row=5, column=1, padx=20)
+
+#Create Text Box Labels
+
+f_name_label=Label(root, text="First Name")
+f_name_label.grid(row=0, column=0)
+
+l_name_label=Label(root, text="Last Name")
+l_name_label.grid(row=1, column=0)
+
+address_label=Label(root, text="Address")
+address_label.grid(row=2, column=0)
+
+city_label=Label(root, text="City")
+city_label.grid(row=3, column=0)
+
+state_label=Label(root, text="State")
+state_label.grid(row=4, column=0)
+
+zipcode_label=Label(root, text="Zip code")
+zipcode_label.grid(row=5, column=0)
+
+#Create Submit Button
+
+submit_btn=Button(root, text="Add record to database", command=submit)
+submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
+root.mainloop()
